e0b5e06409883d95b0b2cfbef8b333e3dce61be9
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-03-21 22:00:35 +0100

    displayed hamburgeer button with animation

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// Selected the two navigation bars 
+// Selected the two navigation bars
 const nav1 = document.getElementById("nav1");
 const nav2 = document.getElementById("nav2");
 
-document.addEventListener ('scroll', function(){
-    if (window.pageYOffset > 0) {
-        nav1.style.display = "none";
-        nav2.style.display = "flex";
-        nav2.style.position ="fixed";
-        nav2.style.top ="0";
-    } else {
-        nav1.style.display ="flex";
-        nav2.style.display = "none";
-    }
-})
+document.addEventListener("scroll", function () {
+  if (window.pageYOffset > 0) {
+    nav1.style.display = "none";
+    nav2.style.display = "flex";
+    nav2.style.position = "fixed";
+    nav2.style.top = "0";
+  } else {
+    nav1.style.display = "flex";
+    nav2.style.display = "none";
+  }
+});
+
+// Script pour la transfo du hamburger button les amis
+function myFunction(x) {
+  x.classList.toggle("change");
+}
